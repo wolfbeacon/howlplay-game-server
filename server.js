3dc12d0e0940daa7dad9cc77b07b325738bda900
Frederic Pun <frederic.pun@gmail.com>
2018-03-15 03:18:06 -0400

    HOW-39 DONE (New branch for new code)

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const WebSocket = require('ws');
 const Storage   = require('./lib/storage');
 const Driver    = require('./lib/howlplay-ws-driver');
-
+const Queue     = require('./lib/functionQueue');
 const config    = require('./config/config');
+const util      = require('./lib/util');
 
 // Initialize variables
 let storage              = new Storage();
@@ -11,7 +12,6 @@ let connectionId         = 0;
 const wss = new WebSocket.Server({ port: config.port }, ()=> {
     console.log("Started Listening On Port:", config.port);
 });
-
 
 wss.on('connection', (ws) => {
     console.log("New connection ...");
@@ -81,3 +81,63 @@ wss.on('connection', (ws) => {
         }).catch(() => {});
     }, 1000);
 });
+
+// Broadcasting function
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    Driver.emitters.scoreGameEmitter(data.slice(1)).then((buff) => {
+      client.send(buff);
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+};
+
+async function batchHandler() {
+  async function buildBatch() {
+    let payload = [];
+    let data = [];
+    await storage.data.participants.find({}).then((docs, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        data = docs;
+      }
+    });
+
+    // Go through all users, and get the new answers, move pointer to indicate checked
+    console.log(data);
+    for (index in data) {
+      let user = data[index];
+      let i = user.sentIndex;
+      let answers = user.answers;
+      let nickname = [];
+      for (let j = 0, strLen = user.nickname.length; j < strLen; j++) {
+          nickname.push(user.nickname.charCodeAt(j));
+      }
+      // If there are more answers for user, put into payload
+      if (i != answers.length) {
+
+        payload = payload.concat([255], Array.from(nickname), [255, i], answers.slice(i));
+        await storage.data.participants.update({_id: user._id}, {$inc: { sentIndex: answers.length }});
+      }
+    }
+
+    console.log("payload built");
+    return new Promise(resolve => resolve(payload));
+  }
+
+  return Queue.addToQueue(buildBatch.bind(this));
+}
+
+let batchInterval = setInterval(async () => {
+    // If we haven't seen a ping in 4 seconds, set it to dead, and stop ping.
+    batchHandler().then(function(payload) {
+      console.log("payload: " + payload);
+      if (payload != []) {
+        console.log("BROADCASTING");
+        payload[0] = 11;
+        wss.broadcast(new Uint8Array(payload));
+      }
+    });
+}, 5000);
